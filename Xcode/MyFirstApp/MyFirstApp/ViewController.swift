2801aadbb2c0ed1f2fcabb59552a6741e15e58e1
Esashika.K <Kento-E@users.noreply.github.com>
2018-01-02 00:54:38 +0900

    How to use Xcode

---
+++ Xcode/MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Kento_E on 2018/01/01.
+//  Copyright © 2018年 Kento_E. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
