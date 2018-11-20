1b7eff3ba31529e40809974c5de5226685f07438
shika-e <Kento-E@users.noreply.github.com>
2018-11-20 12:39:37 +0900

    [update] fix some bugs & styles.

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -1,24 +1,22 @@
-$(function() {
-var canvas = document.getElementById('canvas');
-if (!canvas || !canvas.getContext) return false;
+$(function () {
+	var canvas = document.getElementById('canvas');
+	if (!canvas || !canvas.getContext) return false;
 	var ctx = canvas.getContext('2d'),
-			startX,
-			startY,
-			x,
-			y,
-			borderWidth = ($('#canvas').outerWidth() - $('#canvas').innerWidth())/2,
-			isDrawing = false;
-	$('#canvas').bind('touchstart',function(e){
-		isDrawing = true;
-		event.preventDefault();
-		startX = e.changedTouches[0].pageX;
-		startY = e.changedTouches[0].pageY;
-	})
-		.bind('touchmove',function(e){
+		startX,
+		startY,
+		x,
+		y,
+		borderWidth = ($('#canvas').outerWidth() - $('#canvas').innerWidth()) / 2,
+		isDrawing = false;
+	$('#canvas').mousedown(function (e) {
+			isDrawing = true;
+			startX = e.pageX - $(this).offset().left - borderWidth;
+			startY = e.pageY - $(this).offset().top - borderWidth;
+		})
+		.mousemove(function (e) {
 			if (!isDrawing) return;
-			event.preventDefault();
-			x = e.changedTouches[0].pageX - $(this).offset().left - borderWidth;
-			y = e.changedTouches[0].pageY - $(this).offset().top - borderWidth;
+			x = e.pageX - $(this).offset().left - borderWidth;
+			y = e.pageY - $(this).offset().top - borderWidth;
 			ctx.beginPath();
 			ctx.moveTo(startX, startY);
 			ctx.lineTo(x, y);
@@ -26,32 +24,33 @@ if (!canvas || !canvas.getContext) return false;
 			startX = x;
 			startY = y;
 		})
-		.bind('touchend',function(){
+		.mouseup(function () {
 			isDrawing = false;
 		})
-		.bind('touchcancel',function(){
+		.mouseleave(function () {
 			isDrawing = false;
-	});
-	$('#penColor').change(function(){
+		});
+	$('#penColor').change(function () {
 		ctx.strokeStyle = $(this).val();
 	});
-	$('#penWidth').change(function(){
+	$('#penWidth').change(function () {
 		ctx.lineWidth = $(this).val();
 	});
-	function clear(){
+
+	function clear() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 	};
-	$('#erase').click(function(){
-		if(!confirm('本当に消去しますか？')) return;
+	$('#erase').click(function () {
+		if (!confirm('本当に消去しますか？')) return;
 		clear();
 	});
-	$('#save').click(function(){
-	//  var timeStamp = new Date().format('yy-mm-dd-HH-mm-ss');
+	$('#save').click(function () {
+		//  var timeStamp = new Date().format('yy-mm-dd-HH-mm-ss');
 		var img = $('<img>').attr({
 			src: canvas.toDataURL()
 		});
 		var link = $('<a>').attr({
-			href: canvas.toDataURL().replace('image/png','application/cotet-stream'),
+			href: canvas.toDataURL().replace('image/png', 'application/cotet-stream'),
 			download: 'canvas_' + moment().format('YY-MM-DD-HH-mm-ss') + '.png'
 		});
 		$('#gallery').append(link.append(img.addClass('thumbs')));
